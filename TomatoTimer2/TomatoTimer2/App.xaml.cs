58c3978da557271b57fd30b68339db0933ecceee
Rafał Major <rmaj@demant.com>
2021-11-18 22:15:30 +0100

    Start prism project

--- TomatoTimer2/TomatoTimer2/App.xaml.cs
+++ TomatoTimer2/TomatoTimer2/App.xaml.cs
@@ -1,17 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Prism.Ioc;
 using System.Windows;
+using TomatoTimer2.Views;
 
 namespace TomatoTimer2
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
     }
 }
